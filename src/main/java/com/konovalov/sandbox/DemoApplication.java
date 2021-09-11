143cf5bcee1b837a1ef0e596fd5cb607fc752d6c
mikekonovalov21 <konovalovmisha@gmail.com>
2021-09-11 10:34:19 +0300

    Changed massage

--- src/main/java/com/konovalov/sandbox/DemoApplication.java
+++ src/main/java/com/konovalov/sandbox/DemoApplication.java
@@ -5,6 +5,6 @@ public class DemoApplication
     public static void main( String[] args )
     {
 
-        System.out.println("Hello git");
+        System.out.println("I created new class");
     }
 }
