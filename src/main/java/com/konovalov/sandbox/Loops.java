f2d7e4e6f0cd75532d835f521433eb316051fa37
mikekonovalov21 <konovalovmisha@gmail.com>
2021-09-12 11:17:05 +0300

    Added for, do, do_while loops but in one main method

---
+++ src/main/java/com/konovalov/sandbox/Loops.java
@@ -0,0 +1,19 @@
+package com.konovalov.sandbox;
+
+public class Loops {
+    public static void main (String[] args){
+      //  for (int i = 1; i <= 10; i++) {
+            //System.out.println(i);
+//        int j = 0;
+//        while (j < 10) {
+//            j += 1;
+//            System.out.println(j);
+//        }
+        int i = 1;
+        do{
+            i++;
+        } while (i<=10);
+        System.out.println(i);
+    }
+
+}
